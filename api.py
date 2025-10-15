11fc0cf3eb79ab196d096d1cd25f457350001564
Shankar Bhattarai <shankarbhattarai2063@gmail.com>
2025-10-15 20:46:46 +0545

    fix: allow frontend origins through cors

--- api.py
+++ api.py
@@ -140,7 +140,7 @@ app = FastAPI(title="CricXI API", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
