c79407fe4f8cc079e08b277b15461e3c3c3fa250
Shankar Bhattarai <shankarbhattarai2063@gmail.com>
2025-11-07 12:01:09 +0545

    feat: add fallback squad headshots

---
+++ front-end/src/data/players.js
@@ -0,0 +1,177 @@
+export const squads = {
+  asia: {
+    title: "Australia XI",
+    subtitle: "",
+    background: {
+      from: "#0d35a3",
+      to: "#011e66",
+    },
+    players: [
+      {
+        name: "Travis Head",
+        role: "Opener",
+        image: "https://i.imgur.com/bslTpXq.png",
+      },
+      {
+        name: "David Warner",
+        role: "Opener",
+        image: "https://i.imgur.com/1X8H5bC.png",
+      },
+      {
+        name: "Mitchell Marsh (C)",
+        role: "Captain · All-rounder",
+        image: "https://i.imgur.com/YUh4S3N.png",
+      },
+      {
+        name: "Steven Smith",
+        role: "Middle-order batter",
+        image: "https://i.imgur.com/zpZThx9.png",
+      },
+      {
+        name: "Glenn Maxwell",
+        role: "All-rounder",
+        image: "https://i.imgur.com/shfQwqs.png",
+      },
+      {
+        name: "Marcus Stoinis",
+        role: "All-rounder",
+        image: "https://i.imgur.com/5J2A2IJ.png",
+      },
+      {
+        name: "Tim David",
+        role: "Finisher",
+        image: "https://i.imgur.com/8zKnW1r.png",
+      },
+      {
+        name: "Josh Inglis (WK)",
+        role: "Wicketkeeper · Batter",
+        image: "https://i.imgur.com/0Sxnr0K.png",
+      },
+      {
+        name: "Pat Cummins",
+        role: "Fast bowler",
+        image: "https://i.imgur.com/iu0JyPj.png",
+      },
+      {
+        name: "Mitchell Starc",
+        role: "Left-arm quick",
+        image: "https://i.imgur.com/5Rdhltx.png",
+      },
+      {
+        name: "Adam Zampa",
+        role: "Leg-spinner",
+        image: "https://i.imgur.com/lXRAEmB.png",
+      },
+    ],
+    substitutes: [
+      {
+        name: "Matthew Wade",
+        role: "WK Finisher",
+      },
+      {
+        name: "Cameron Green",
+        role: "All-rounder",
+      },
+      {
+        name: "Josh Hazlewood",
+        role: "Fast bowler",
+      },
+      {
+        name: "Sean Abbott",
+        role: "Seam bowling all-rounder",
+      },
+      {
+        name: "Ashton Agar",
+        role: "Left-arm spinner",
+      },
+    ],
+  },
+  world: {
+    title: "India XI",
+    subtitle: "",
+    background: {
+      from: "#b3141a",
+      to: "#690406",
+    },
+    players: [
+      {
+        name: "Rohit Sharma (C)",
+        role: "Captain · Opener",
+        image: "https://i.imgur.com/viH70v2.png",
+      },
+      {
+        name: "Shubman Gill",
+        role: "Opener",
+        image: "https://i.imgur.com/Jmd0dGd.png",
+      },
+      {
+        name: "Virat Kohli",
+        role: "Top-order batter",
+        image: "https://i.imgur.com/OtzqOQg.png",
+      },
+      {
+        name: "Shreyas Iyer",
+        role: "Middle-order batter",
+        image: "https://i.imgur.com/uMbx22w.png",
+      },
+      {
+        name: "Suryakumar Yadav",
+        role: "Finisher",
+        image: "https://i.imgur.com/GgE2XnL.png",
+      },
+      {
+        name: "Hardik Pandya",
+        role: "All-rounder",
+        image: "https://i.imgur.com/oY2YLGu.png",
+      },
+      {
+        name: "Rishabh Pant (WK)",
+        role: "Wicketkeeper · Batter",
+        image: "https://i.imgur.com/m2Pj9pY.png",
+      },
+      {
+        name: "Ravindra Jadeja",
+        role: "All-rounder",
+        image: "https://i.imgur.com/50oKPhQ.png",
+      },
+      {
+        name: "Kuldeep Yadav",
+        role: "Wrist-spinner",
+        image: "https://i.imgur.com/37TnJFs.png",
+      },
+      {
+        name: "Jasprit Bumrah",
+        role: "Fast bowler",
+        image: "https://i.imgur.com/p4pBiDJ.png",
+      },
+      {
+        name: "Mohammed Siraj",
+        role: "Fast bowler",
+        image: "https://i.imgur.com/A7fEe3H.png",
+      },
+    ],
+    substitutes: [
+      {
+        name: "KL Rahul",
+        role: "Top-order batter",
+      },
+      {
+        name: "Ishan Kishan",
+        role: "WK Batter",
+      },
+      {
+        name: "Axar Patel",
+        role: "All-rounder",
+      },
+      {
+        name: "Mohammed Shami",
+        role: "Fast bowler",
+      },
+      {
+        name: "Washington Sundar",
+        role: "Off-spinner",
+      },
+    ],
+  },
+};
+
